7b3f701daa398dd674f78167fee14ed61c548efa
Alehub369 <lievanoalejandro@gmail.com>
2023-11-08 22:57:15 -0500

    Función de guardar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
 
 class PostController extends Controller
 {
@@ -19,9 +22,20 @@ class PostController extends Controller
         return view('posts.create', ['post' => $post]);
     }
 
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug'  => Str::slug($title),
+            'body'  => $request->body,
+        ]);
+        return redirect()->route('posts.edit', $post);
+    }
+
     public function edit(Post $post)
     {
         return view('posts.edit', ['post' => $post]);
+
     }
 
     public function destroy(Post $post)

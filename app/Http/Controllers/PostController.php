6561c33598478c534f9a7f31808317b150db0fff
Alehub369 <lievanoalejandro@gmail.com>
2023-11-08 23:49:32 -0500

    Validación

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,6 +24,11 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'body'  => 'required',
+        ]);
+
         $post = $request->user()->posts()->create([
             'title' => $title = $request->title,
             'slug'  => Str::slug($title),
@@ -40,6 +45,11 @@ class PostController extends Controller
 
     public function update(Request $request, Post $post)
     {
+        $request->validate([
+            'title' => 'required',
+            'body'  => 'required',
+        ]);
+        
         $post->update([
             'title' => $title = $request->title,
             'slug'  => Str::slug($title),
